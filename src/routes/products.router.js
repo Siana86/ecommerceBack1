3c445ce4cf3fb2ca2164ab7d72ac066c0896bd2b
Anais Marcano <amarcano2786@gmail.com>
2025-07-01 20:57:21 -0300

    Crear products y cart routers

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -35,4 +35,57 @@ productsRouter.post("/", async (req, res) => {
     }
 });
 
+//GET: Get products
+productsRouter.get("/", (req, res) => {
+
+    res.json({ status: "success", message: "Solicitud recibida" });
+});
+
+productsRouter.get("/api/products", async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+
+        res.status(200).json({ status: "success", products });
+    } catch (error) {
+        res.status(500).json({ status: "error" }); //TO DO: mejorar respuesta del error
+    }
+});
+
+
+//DELETE: Delete product by id
+productsRouter.delete("/api/products/:pid", async (req, res) => {
+    try {
+        const productId = req.params.pid;
+        const products = await productManager.deleteProductById(productId);
+        res.status(200).json({ status: "success", products });
+    } catch (error) {
+        res.status(500).json({ status: "error" }); //TO DO: mejorar respuesta del error
+    }
+});
+
+//PUT: Updtae product by id
+productsRouter.put("/api/products/:pid", async (req, res) => {
+    try {
+        const productId = req.params.pid;
+        const updatedData = req.body;
+
+        const products = await productManager.updateProductById(productId, updatedData);
+        res.status(200).json({ status: "success", products });
+    } catch (error) {
+        res.status(500).json({ status: "error" }); //TO DO: mejorar respuesta del error
+    }
+});
+
+//GET: getProductById 
+productsRouter.get("/api/products/:pid", async (req, res) => {
+    try {
+        const productId = req.params.pid;
+        const product = await productManager.getProductById(productId);
+        res.status(200).json({ status: "success", product });
+    } catch (error) {
+        res.status(500).json({ status: "error" }) //TO DO: mejorar respuesta del error 
+    }
+});
+
+
 export default productsRouter;
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -2,7 +2,6 @@ import express  from "express";
 import ProductManager from "../ProductManager.js";
 
 
-
 const viewsRouter = express.Router();
 const productManager = new ProductManager("./src/products.json");
 
@@ -10,13 +9,21 @@ const productManager = new ProductManager("./src/products.json");
 
 //Endpoints 
 viewsRouter.get("/" , async (req, res) => {
-    const products = await productManager.getProducts();
-    res.render("home", {products});
+    try {
+        const products = await productManager.getProducts();
+        res.render("home", {products});
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
 })
 
 viewsRouter.get("/realTimeProducts", async (req , res) => {
-    const products = await productManager.getProducts();
-    res.render("realTimeProducts",{products});
+    try {
+        const products = await productManager.getProducts();
+        res.render("realTimeProducts",{products});
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
 })
 
 export default viewsRouter;
